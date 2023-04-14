01e5784e4ce44c40672105376122e236a7b1fbcb
Antonela Acosta <ann.acosta388@gmail.com>
2023-04-13 21:13:43 -0300

    GifExpertApp

---
+++ Section 6/src/GifExpertApp.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { AddCategory,GifGrid } from './components';
+
+export const GifExpertApp = ()=>{
+
+    const [categories, setCategories] = useState (['One Punch'])
+    
+    const onAddCategory = (newCategory) => {
+        //categories.push(newCategory);
+        if ( categories.includes(newCategory) ) return;
+        setCategories([ newCategory, ...categories ]);
+        //setCategories(cat=>[...cat, 'Valorant'])
+    
+    }
+
+
+    return(
+        <> 
+        {/*TITULO*/}
+        <h1>GifExpertApp</h1>
+
+        {/* IMPUT*/} 
+        <AddCategory 
+        //setCategories={setCategories}
+        onNewCategory={ (value) => onAddCategory(value) }
+        />
+
+        {/*LISTADO*/}
+        {/*el map me permite barrer los elemnentos del arreglo*/}
+     
+        {categories.map( (category) => (
+                <GifGrid
+                    key={category}
+                    category={ category}/>
+
+                
+            ))
+        }
+        </>
+        
+    )
+}
---
+++ Section 6/src/components/AddCategory.jsx
@@ -0,0 +1,40 @@
+
+import { useState } from 'react';
+
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    const onInputChange = ({ target }) => {
+        setInputValue( target.value );
+    }
+    
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        if( inputValue.trim().length <= 1) return;
+
+        //setCategories(categories=> [inputValue, ...categories])
+        setInputValue('');
+        onNewCategory(inputValue.trim());
+        
+
+    }
+
+
+
+    return(
+        <form onSubmit={onSubmit}>
+          <input
+             type="text"
+             placeholder="Buscar gifs"
+             value={inputValue}
+             onChange={ onInputChange }
+        
+          />
+        </form>
+    )
+}
+//Cada componente puede tener su propio estado, puede tener sus propios hooks
+//Dentro del input estan las props.
+//El form es el unico elemento que estoy regresando.
